35969ea675e529ab722ea0f88f272d5568736a5a
Tamikon <richichichi2207@gmail.com>
2024-04-03 22:12:22 +0300

    Реализовал возможность просмотра каждого товара в отдельности с его описанием

---
+++ ASP_Homework_Product/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductList productList;
+
+        public ProductController()
+        {
+            productList = new ProductList();
+        }
+        public string Index(int id)
+        {
+            var product = productList.TryGetById(id);
+            if (product == null)
+            {
+                return "Ноутбука нет на складе";
+            }
+            return $"{product}\n{product.Description}";
+        }
+    }
+}
--- ASP_Homework_Product/ProductList.cs
+++ ASP_Homework_Product/ProductList.cs
@@ -5,7 +5,7 @@ namespace ASP_Homework_Product
 {
     public class ProductList
     {
-        List<Product> listOfProducts = new List<Product>()
+        private static List<Product> listOfProducts = new List<Product>()
         {
           new Product("ASUS ROG Zephyrus Duo 16", "2560х1600, AMD Ryzen 9 7945HX 2.5 ГГц, NVIDIA GeForce RTX 4090", 499990),
           new Product("GIGABYTE Gб", "1920х1200, Intel Core i7 12650H 2.3 ГГц, NVIDIA GeForce RTX 4050", 105990),
@@ -15,6 +15,17 @@ namespace ASP_Homework_Product
         public List<Product> GetProducts()
         {
             return listOfProducts;
+        }
+        public Product TryGetById(int id)
+        {
+            foreach (var product in listOfProducts)
+            {
+                if (product.Id == id)
+                {
+                    return product;
+                }
+            }
+            return null;
         }
     }
 }
