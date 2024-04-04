35f95dedad55b4ecf073b4f58d86798019e2f9a8
Tamikon <richichichi2207@gmail.com>
2024-04-05 01:06:04 +0300

    Реализовал возможность добавления товаров в корзину. где пользователь может посмотреть список желаемых товаров с ценой, количеством. названием и стоимостью каждого, а также итоговую сумму всего заказа.

---
+++ ASP_Homework_Product/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductList productList;
+
+        public CartController()
+        {
+            productList = new ProductList();
+        }
+
+        public IActionResult Index()
+        {
+            var cart = CartsRes.TryGetByUserId(Constants.UserId);
+            return View(cart);
+        }
+        
+        public IActionResult Add(int productId)
+        {
+            var product = productList.TryGetById(productId);
+            CartsRes.Add(product, Constants.UserId);
+            return RedirectToAction("Index");
+        }
+    }
+}
